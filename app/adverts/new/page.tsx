28321e9af900c3633dd6934b2d3b6a534f2b0971
sevvalsaygat <sevvallsstt@gmail.com>
2024-01-23 22:09:52 +0300

    feat: create page structure

--- app/adverts/new/page.tsx
+++ app/adverts/new/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <Link href="/adverts/new">
-      <button>Home</button>
+    <Link href="/">
+      <button>New Adverts</button>
     </Link>
   );
 }
